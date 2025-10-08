e294761cc69c5d5a91acaed3665bd686cc55da86
Glitcherobrine <50085705+HEROBRINE100008@users.noreply.github.com>
2025-10-08 23:27:18 +0000

    new

---
+++ Programacion/C#/3_comments/Program.cs
@@ -0,0 +1,5 @@
+﻿// Esto es un comentario
+Console.WriteLine("Hola, mundo!"); //Este tambien es un comentario
+/* este es un comentario de varias lineas
+   que puede ocupar mas de una linea
+   y termina aqui */
