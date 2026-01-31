8f2d39b2a59a5a5d4c683e097b4597abe6c17382
HEROBRINE100008 <ismael011913@gmail.com>
2026-01-30 20:09:09 -0400

    django2

--- Programacion/Python/Django/myapp/views.py
+++ Programacion/Python/Django/myapp/views.py
@@ -1,9 +1,20 @@
-
-from django.http import HttpResponse
-
+from django.http import HttpResponse, JsonResponse
+from .models import Project, Task
+from django.shortcuts import get_object_or_404, render
 # Create your views here.
-def hello(request):
-    return HttpResponse("<h2>Hello World<h2/>")
+def index(request):
+    return render(request, 'index.html')
+
+def hello(request, username):
+    return HttpResponse("<h2>Hello %s<h2/>" % username )
 
 def about(request):
     return HttpResponse('About')
+
+def projects(request):
+    projects = list(Project.objects.values())
+    return JsonResponse(projects , safe=False)
+
+def tasks(request, id):
+    task = get_object_or_404(Task, id=id)
+    return HttpResponse('tasks: %s' % task.title)
