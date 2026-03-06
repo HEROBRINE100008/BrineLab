86ad4b300bd9fec73c9527b3f3965de121344254
HEROBRINE100008 <ismael011913@gmail.com>
2026-03-06 12:27:45 -0400

    Java Fix File

--- Programacion/Java/If.java
+++ Programacion/Java/If.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 
 public class If {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        try (Scanner sc = new Scanner(System.in)) {
         System.out.print("Pon cuanto tiene la chica");
         int edad = sc.nextInt();
         if (edad >= 18) {
             System.out.println("ta muy grande bro");
         } else {
             System.out.println("exaito");
-        }
+        }}
     }
 }
