063fb1b3724a3f5de37e51b502231888ae7a6149
HEROBRINE100008 <ismael011913@gmail.com>
2026-03-11 19:16:23 -0400

    mouredev sync

---
+++ Programacion/Java/mouredev/DataTypes.java
@@ -0,0 +1,17 @@
+public class DataTypes {
+	
+	public static void main(String[] args) {
+		
+		int myInt = 19;
+		System.out.println(myInt);
+		
+		double myDouble = 1.60;
+		System.out.println(myDouble);
+		
+		char myChar = 'H';
+		System.out.println(myChar);
+		
+		boolean myBoolean = true
+		
+		}
+	}
---
+++ Programacion/Java/mouredev/VariablesAndConstants.java
@@ -0,0 +1,25 @@
+public class VariablesAndConstants {
+	
+	public static void main(String[] args) {
+		
+		
+		String name = "Ismael";
+		System.out.println(name);
+		
+		name = "Rodriguez";
+		System.out.println(name);
+		
+		int age = 19;
+		System.out.println(age);
+		
+		final String EMAIL = "ismael011913@gmail.com";
+		System.out.println(EMAIL);
+		
+		var firstName = "Junior";
+		System.out.println(firstName);
+		
+		var year = 2026;
+		System.out.println(year);
+		
+		}
+	}
