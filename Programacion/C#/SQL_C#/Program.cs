2e5a72c91379737b8274f4030d0ce99c90ddd121
Glitcherobrine <50085705+HEROBRINE100008@users.noreply.github.com>
2026-04-13 22:40:27 +0000

    Codespace sync and Calculadora create

---
+++ Programacion/C#/SQL_C#/Program.cs
@@ -0,0 +1,18 @@
+﻿int Ca(){
+    Console.WriteLine("Introduce un número: ");
+    while(true){
+        if (int.TryParse(Console.ReadLine(), out int numero)) {
+                return numero;
+        } else
+        {
+            Console.WriteLine("No ta valido eso pai :("); 
+            Console.WriteLine("Introduce un número: ");  
+        }
+    }
+}
+
+int numero_1 = entrada();
+int numero_2 = entrada();
+
+Console.WriteLine(numero_1);
+Console.WriteLine(numero_2);
