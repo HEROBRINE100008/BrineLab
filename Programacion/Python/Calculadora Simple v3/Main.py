e46c81e08348bb980cdfa54f99323d0bd67dfb31
HEROBRINE100008 <ismael011913@gmail.com>
2026-05-04 22:23:44 -0400

    chromebook sync

--- Programacion/Python/Calculadora Simple v3/Main.py
+++ Programacion/Python/Calculadora Simple v3/Main.py
@@ -1,34 +1,35 @@
 from calculadora import suma, resta, multiplicación, división
 
-#Menú
+# Menú
 Operacion = 0
 
-#Pregunta al usuario que tipo de operación quiere hacer
+# Pregunta al usuario que tipo de operación quiere hacer
 
 while True:
-    
-    Operacion = int(input("Elige Una Opción 1. Sumar 2. Restar 3. Multiplicar 4. Dividir 5. Salir: "))
-    
+    menu = """
+1. Sumar
+2. Restar
+3. Multiplicar
+4. Dividir
+5. Salir
+Elige Una Opción: """
+    Operacion = int(input(menu))
     if Operacion == 1:
-          v1 = int(input("Dame el primer valor: "))
-          v2 = int(input("Dame el segundo valor: "))
-          print("Tu resultado es: ",suma(v1,v2))
-    
+        v1 = int(input("Dame el primer valor: "))
+        v2 = int(input("Dame el segundo valor: "))
+        print("Tu resultado es: ", suma(v1, v2))
     elif Operacion == 2:
-          v1 = int(input("Dame el primer valor: "))
-          v2 = int(input("Dame el segundo valor: "))
-          print("Tu resultado es: ",resta(v1,v2))
-    
+        v1 = int(input("Dame el primer valor: "))
+        v2 = int(input("Dame el segundo valor: "))
+        print("Tu resultado es: ", resta(v1, v2))
     elif Operacion == 3:
-          v1 = int(input("Dame el primer valor: "))
-          v2 = int(input("Dame el segundo valor: "))
-          print("Tu resultado es: ",multiplicación(v1,v2))
-    
+        v1 = int(input("Dame el primer valor: "))
+        v2 = int(input("Dame el segundo valor: "))
+        print("Tu resultado es: ", multiplicación(v1, v2))
     elif Operacion == 4:
-          v1 = int(input("Dame el primer valor: "))
-          v2 = int(input("Dame el segundo valor: "))
-          print("Tu resultado es: ",división(v1,v2))
-    
+        v1 = int(input("Dame el primer valor: "))
+        v2 = int(input("Dame el segundo valor: "))
+        print("Tu resultado es: ", división(v1, v2))
     elif Operacion == 5:
         print("Saliste del programa.")
         break
