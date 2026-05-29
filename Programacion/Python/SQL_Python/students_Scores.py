0e7f8d905fde6ac3657a9dd029db8985f3200baa
HEROBRINE100008 <ismael011913@gmail.com>
2026-05-29 00:14:48 -0400

    arch sync

--- Programacion/Python/SQL_Python/students_Scores.py
+++ Programacion/Python/SQL_Python/students_Scores.py
@@ -3,46 +3,50 @@ from show import show_Grades
 from add import add_Stu
 from approval import appr
 
+
 def init_db():
-	con = sqlite3.connect("Sql.db")
-	cur = con.cursor()
+    con = sqlite3.connect("Sql.db")
+    cur = con.cursor()
 
-	cur.execute("""
-	CREATE TABLE IF NOT EXISTS "Notas" (
-		"ID"	INTEGER NOT NULL UNIQUE,
-		"Students"	TEXT NOT NULL,
-		"P1"	INTEGER NOT NULL,
-		"P2"	INTEGER NOT NULL,
-		"P3"	INTEGER NOT NULL,
-		"P4"	INTEGER NOT NULL,
-		PRIMARY KEY("ID" AUTOINCREMENT)
-	)""")
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS "Notas" (
+        "ID"	INTEGER NOT NULL UNIQUE,
+        "Students"	TEXT NOT NULL,
+        "P1"	INTEGER NOT NULL,
+        "P2"	INTEGER NOT NULL,
+        "P3"	INTEGER NOT NULL,
+        "P4"	INTEGER NOT NULL,
+        PRIMARY KEY("ID" AUTOINCREMENT)
+    )""")
 
-	con.commit()
-	con.close()
+    con.commit()
+    con.close()
+
 
 def menu():
-	init_db()
-	
-	while True:
-		print("\n-----Notas De Los Estudiantes-----")
-		print("1. Ingresar Estudiante")
-		print("2. Ver Notas Guardadas")
-		print("3. Resultados")
-		print("4. Salir")
+    init_db()
 
-		option = input("Elige una opción: ")
-		
-		match option:
-			case '1':
-				add_Stu()
-			case '2':
-				show_Grades()
-			case '3':
-				appr()
-			case '4':
-				break
-			case _:
-				print("\nError: Opcion no valida")
+    while True:
+        print("\n-----Notas De Los Estudiantes-----")
+        print("1. Ingresar Estudiante")
+        print("2. Ver Notas Guardadas")
+        print("3. Resultados")
+        print("4. Salir")
+
+        option = input("Elige una opción: ")
+
+        match option:
+            case '1':
+                add_Stu()
+            case '2':
+                show_Grades()
+            case '3':
+                appr()
+            case '4':
+                break
+            case _:
+                print("\nError: Opcion no valida")
+
+
 if __name__ == "__main__":
-	menu()
+    menu()
