ec930acfdd8dde27a04fb15954b6f53522f9599e
HEROBRINE100008 <ismael011913@gmail.com>
2026-06-17 02:47:49 -0400

    arch sync

--- Programacion/Python/derail_valley/calc.py
+++ Programacion/Python/derail_valley/calc.py
@@ -1,12 +1,28 @@
 from train import data
+from main import srchCatalog
 
 
-def calcWeight():
-
+def calcWeight(trainId, trainList):
+    for t in trainList:
+        if t.ID == trainId:
+            return t
 
 
 if __name__ == "__main__":
     catalog = data()
-    for i in catalog:
-        calcWeight() 
+    trainID = 2
 
+    trainFound = srchCatalog(trainID, catalog)
+    loadWeight = 200
+
+    if trainFound is not None:
+        trainFound.maxWeight -= loadWeight
+
+        if trainFound.maxWeight >= 0:
+            print("El peso está en rango de capacidad de carga")
+            print(f"Restante: {trainFound.maxWeight}t")
+        else:
+            print(f"capacidad de carga sobrepasado por {}")
+
+    else:
+        print("Error:Objeto Nulo")
