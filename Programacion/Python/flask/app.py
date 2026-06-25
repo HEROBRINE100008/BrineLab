37f602d1aebde6289627d70e3ce8d3075e2e3524
HEROBRINE100008 <ismael011913@gmail.com>
2026-06-25 00:42:17 -0400

    arch sync

--- Programacion/Python/flask/app.py
+++ Programacion/Python/flask/app.py
@@ -1,5 +1,4 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -8,6 +7,13 @@ app = Flask(__name__)
 def page():
     return render_template("index.html")
 
+
+@app.route("/recibir", methods=["POST"])
+def recibir():
+    texto = request.form["xd"]
+    print(f"La palabra que dijo el singasumadre del usuario fue:{texto}")
+    
+    return f"<h1>Entonce dijite {texto}</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
