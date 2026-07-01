a2247dc642681f9711f837f532baeb170429f274
HEROBRINE100008 <ismael011913@gmail.com>
2026-07-01 17:51:21 -0400

    arch sync

--- Programacion/Python/conversor_de_temperatura/calc.py
+++ Programacion/Python/conversor_de_temperatura/calc.py
@@ -1,13 +1,21 @@
-def F_a_C()
+# Calculos de Farenheit
+def F_a_C(f): return (f - 32) * 5 / 9
+def F_a_K(f): return (f - 32) * 5 / 9 + 273.15
+# Calculos de Celsius
+def C_a_F(c): return (c * 9 / 5) + 32
+def C_a_K(c): return c + 273.15
+# Calculos de Kelvin
+def K_a_F(k): return (k - 273.15) * 9 / 5 + 32
+def K_a_C(k): return k - 273.15
 
 
 if __name__ == "__main__":
     # Farenheit
-    F_a_C(79)
-    F_a_K(79)
+    print(F_a_C(float(79)))
+    print(F_a_K(float(79)))
     # Celsius
-    C_a_F(26)
-    C_a_K(26)
+    print(C_a_F(float(26)))
+    print(C_a_K(float(26)))
     # Kelvin
-    K_a_F(300)
-    K_a_C(300)
+    print(K_a_F(float(300)))
+    print(K_a_C(float(300)))
--- Programacion/Python/conversor_de_temperatura/main.py
+++ Programacion/Python/conversor_de_temperatura/main.py
@@ -9,10 +9,16 @@ def menu():
               2: °C
               3. K
               4. Salir""")
-
+        
+        option = input("Ingrese un numero(1-4): ")
         match option:
             case '1':
-                
+                print("""Seleccione una opción
+                      1. °F a °C
+                      2. °F a K""")
+                sub_option = input("Ingrese un número(1-2): ")
+                if sub_option == "1":
+                    
 
 if __name__ == "__main__":
     menu()
