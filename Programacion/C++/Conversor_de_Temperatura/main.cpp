a454090df6c6a92033987dff53fdb64e292684b3
HEROBRINE100008 <ismael011913@gmail.com>
2026-07-01 19:41:56 -0400

    arch sync

--- Programacion/C++/Conversor_de_Temperatura/main.cpp
+++ Programacion/C++/Conversor_de_Temperatura/main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
+#include <ostream>
 
 using namespace std;
 
 int main()
 {
-    cout << "hello world" << endl;
+	int option{0};
+	 while (true){
+		cout << "Selecciona una opción\n1. °F\n2. °C\n3. K\n4. Salir\n";
+		cout << "Ingrese un numero(1-4): ";
+		cin >> option;
 
+		cout << "Se ingresó " << option << "\n";
+		break;
+	}
 }
--- Programacion/Python/conversor_de_temperatura/main.py
+++ Programacion/Python/conversor_de_temperatura/main.py
@@ -9,7 +9,7 @@ def menu():
               2: °C
               3. K
               4. Salir""")
-        
+
         option = input("Ingrese un numero(1-4): ")
         match option:
             case '1':
@@ -18,8 +18,7 @@ def menu():
                       2. °F a K""")
                 sub_option = input("Ingrese un número(1-2): ")
                 if sub_option == "1":
-                    
+
 
 if __name__ == "__main__":
     menu()
-
